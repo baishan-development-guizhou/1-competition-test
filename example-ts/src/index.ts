2b19e48ec888e6c3479d1492f798d511e514b814
阿月很乖 <lizhongyue248@163.com>
2021-01-24 20:36:16 +0800

    feat(Typescript): Add example for Typescript.

---
+++ example-ts/src/index.ts
@@ -0,0 +1,14 @@
+import Koa from 'koa'
+
+class Server {
+  constructor(port: number) {
+    const app = new Koa();
+    app.use( async ( ctx ) => {
+      ctx.body = 'hello koa2'
+    })
+    app.listen(port)
+  }
+}
+
+new Server(3000)
+console.log('[demo] start-quick is starting at port 3000')
