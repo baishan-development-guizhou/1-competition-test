817205aaf083ad97a995857fd94898a878fcc6e9
阿月很乖 <lizhongyue248@163.com>
2021-01-25 21:28:24 +0800

    feat(php): Add example for Php.

---
+++ example-php/main.php
@@ -0,0 +1,33 @@
+<?php
+$_SERVER = array();
+//创建一个tcp套接字,并监听8088端口
+if ($web = stream_socket_server('0.0.0.0:8080', $errno, $errstr)) {
+    while (true) {
+        $conn = @stream_socket_accept($web);
+        if ($conn) {
+            $_SERVER = array();
+            decode(fgets($conn));
+            fwrite($conn, encode("Method:" . $_SERVER['REQUEST_METHOD'] . "\nparams:" . $_SERVER['QUERY_STRING']));
+            fclose($conn);
+        }
+    }
+} else {
+    die($errstr);
+}
+//http协议解码
+function decode($info)
+{
+    global $_SERVER;
+    list($header,) = explode("\r\n\r\n", $info);
+    //将请求头变为数组
+    $header = explode("\r\n", $header);
+    list($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI'], $_SERVER['SERVER_PROTOCOL']) = explode(' ', $header[0]);
+    $_SERVER['QUERY_STRING'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+}
+
+//http协议加密
+function encode($str)
+{
+    $content = "HTTP/1.1 200 OK\r\nServer: vruan_web/1.0.0\r\nContent-Length: " . strlen($str) . "\r\n\r\n{$str}";
+    return $content;
+}
